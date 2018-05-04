76b79cbd0f3864262ea161d0f4be71f9cf175b5a
Molizo <visoiu.mihnea@live.com>
2018-05-04 20:21:12 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+char c[10000];
+
+switch(char m)
+{
+    case m=='I'
+        return 1;
+    case m=='V'
+        return 5;
+    case m=='X'
+        return 10;
+    case m=='L'
+        return 50;
+    case m=='C'
+        return 100;
+    case m=='D'
+        return 500;
+    case m=='M'
+        return 1000;
+}
+
+int shiftRight(int pos)
+{
+    for(int i=0;i<pos;i++)
+        char[i]=char[i+1];
+}
+
+int main()
+{int numar;
+    cin>>numar;
+    while(numar)
+    return 0;
+}
